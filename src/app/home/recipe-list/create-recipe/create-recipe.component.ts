4fbbfe452b145961a81f2e1be7d8930ed1ddfbb1
HWaiChan <hanswai93@gmail.com>
2021-07-20 22:45:25 +0100

    button!

--- src/app/home/recipe-list/create-recipe/create-recipe.component.ts
+++ src/app/home/recipe-list/create-recipe/create-recipe.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { RecipeService } from 'src/app/services/recipe.service';
 import { Recipe } from 'src/app/interfaces/recipes';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-create-recipe',
@@ -8,11 +14,23 @@ import { Recipe } from 'src/app/interfaces/recipes';
   styleUrls: ['./create-recipe.component.css'],
 })
 export class CreateRecipeComponent implements OnInit {
-  constructor(private _recipeServie: RecipeService) {}
+  form = this.fb.group({
+    name: ['', Validators.required],
+    cuisine: ['', Validators.required],
+  });
+  constructor(private _recipeServie: RecipeService, private fb: FormBuilder) {}
 
   ngOnInit(): void {}
 
   createRecipe(recipe: Recipe): void {
     console.log(recipe);
+  }
+
+  onSubmit(): void {
+    const recipe: Recipe = {
+      name: this.form.get('name')?.value,
+      cuisine: this.form.get('cuisine')?.value,
+    };
+    this.createRecipe(recipe);
   }
 }
--- src/app/home/recipe-list/recipe-list.component.ts
+++ src/app/home/recipe-list/recipe-list.component.ts
@@ -1,25 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from 'src/app/interfaces/recipes';
-import { RecipeService } from 'src/app/services/recipe.service'
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipeData: Recipe[] = []
+  recipeData: Recipe[] = [];
   constructor(_recipeService: RecipeService) {
     this.recipeData = _recipeService.getRecipes();
-   }
-
-  ngOnInit(): void {
-    
   }
 
-  createRecipe(): void {
-    console.log("bleh");
-    
-  }
+  ngOnInit(): void {}
 
+  createRecipe(): void {}
 }
